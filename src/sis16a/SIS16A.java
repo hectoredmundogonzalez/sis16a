2d32a505f7ef32a388d9bfdb4784a6a278ed1bfd
hectoredmundogonzalez <hector.gonzalez@itca.edu.sv>
2016-05-27 08:12:03 -0600

    Update SIS16A.java

--- src/sis16a/SIS16A.java
+++ src/sis16a/SIS16A.java
@@ -17,6 +17,7 @@ public class SIS16A {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.print("Hola Otra vez!");
         
     }
     
